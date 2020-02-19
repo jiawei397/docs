ff8fe7172717036189d38656ed39071c40d02d04
jw397 <jw397@126.com>
2020-02-19 16:03:59 +0800

    build: 开始

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '帝之下都',
+  description: 'Just playing around',
+  // theme: '@vuepress/blog'
+  base: '/docs/',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '指南', link: '/foo/' },
+      { text: 'github', link: 'https://github.com/jiawei397' },
+    ]
+  }
+};
---
+++ docs/foo/README.md
@@ -0,0 +1,19 @@
+# [js](./one.html)
+``` js
+export default {
+  name: 'MyComponent',
+  // ...
+}
+```
+
+# [html](./tip.html)
+``` html
+<ul>
+  <li
+    v-for="todo in todos"
+    :key="todo.id"
+  >
+    {{ todo.text }}
+  </li>
+</ul>
+```
