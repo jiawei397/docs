35ace0499fcf71dcb56f452505cb1a9438161719
jw397 <jw397@126.com>
2020-05-24 21:18:41 +0800

    perf(clone): 优化

--- docs/.vuepress/public/js/clone.js
+++ docs/.vuepress/public/js/clone.js
@@ -1,26 +1,12 @@
-function isFunction(obj) {
-  return typeof obj === 'function' && typeof obj.nodeType !== 'number';
-}
-function clone(obj, isDeep, endFun) {
-  if (!obj || typeof obj !== 'object' || (isFunction(endFun) && endFun(obj))) {
+function clone(obj) {
+  if (!obj || typeof obj !== 'object') {
     return obj;
   }
   var c = obj instanceof Array ? [] : {};
   for (var i in obj) {
     var prop = obj[i];
-    if (isDeep && typeof prop === 'object') {
-      if (prop instanceof Array) {
-        c[i] = [];
-        for (var j = 0; j < prop.length; j++) {
-          if (typeof prop[j] !== 'object') {
-            c[i].push(prop[j]);
-          } else {
-            c[i].push(clone(prop[j], isDeep, endFun));
-          }
-        }
-      } else {
-        c[i] = clone(prop, isDeep, endFun);
-      }
+    if (typeof prop === 'object') {
+      c[i] = clone(prop);
     } else {
       c[i] = prop;
     }
